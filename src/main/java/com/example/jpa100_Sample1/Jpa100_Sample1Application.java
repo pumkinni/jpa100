93d437288dc2fd726ae09926a742bf3a4283ea18
pumkinni <cindy4766@naver.com>
2023-05-08 12:37:50 +0900

    Initial commit solution 1~5

---
+++ src/main/java/com/example/jpa100_Sample1/Jpa100_Sample1Application.java
@@ -0,0 +1,13 @@
+package com.example.jpa100_Sample1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jpa100_Sample1Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Jpa100_Sample1Application.class, args);
+  }
+
+}
---
+++ src/main/java/com/example/jpa100_Sample1/controller/SecondController.java
@@ -0,0 +1,26 @@
+package com.example.jpa100_Sample1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SecondController {
+
+  @GetMapping("/hello-spring")
+  public String hello() {
+    return "hello spring";
+  }
+
+  @GetMapping("/hello-rest")
+  public String helloRest() {
+    return "hello rest";
+  }
+
+
+  @GetMapping("/api/helloworld")
+  public String helloRestApi() {
+    return "hello rest api";
+  }
+
+
+}
